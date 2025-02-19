174b6e53e18efa1e7e471b41dd5586c037f487cf
Fenerz07 <hugom2005@free.fr>
2025-02-19 15:58:24 +0100

    ✨| #3 | add HomePage component with rotating welcome text and update styles

--- website/src/app/page.js
+++ website/src/app/page.js
@@ -1,11 +1,13 @@
 import Header from '../components/header';
 import CustomCursor from '../components/customCursor';
+import HomePage from '../components/homePage';
 
 export default function Home() {
   return (
     <>
       <CustomCursor />
       <Header />
+      <HomePage />
     </>
   );
 }
