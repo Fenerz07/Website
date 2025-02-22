8d3c80fc380896abe24c044beffae5484ba5de84
Fenerz07 <hugom2005@free.fr>
2025-02-22 22:36:37 +0100

    🔥| #3 | refactor HomePage component; replace rotating text with animated title and subtitle using framer-motion; remove unused component and styles

--- website/src/app/page.js
+++ website/src/app/page.js
@@ -1,33 +1,30 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { motion } from 'framer-motion';
 import styles from '../styles/homePage.module.css';
 
 export default function HomePage() {
-    const texts = ["Bienvenue", "Welcome", "Witaj"];
-    const [currentText, setCurrentText] = useState(texts[0]);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentText(prevText => {
-                const currentIndex = texts.indexOf(prevText);
-                const nextIndex = (currentIndex + 1) % texts.length;
-                return texts[nextIndex];
-            });
-        }, 2950);
-
-        return () => clearInterval(interval);
-    }, []);
-
     return (
         <div className={styles.homePage}>
-            <h1 className={styles.rotatingText}>{currentText}</h1>
-            <p>Vous êtes sur la page principal de mon portfolio
-                <br/>
-                Vous pouvez naviguer sur les différentes pages en cliquant sur [à remplir]
-                <br/>
-                Bonne visite à vous!
-            </p>
+            
+            <motion.h1
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1.5, ease: "easeOut" }}
+                className={styles.title}
+            >
+                Bienvenue sur mon portfolio
+            </motion.h1>
+
+            <motion.p
+                initial={{ opacity: 0, filter: "blur(10px)" }}
+                animate={{ opacity: 1, filter: "blur(0px)" }}
+                transition={{ duration: 1, ease: "easeOut", delay: 0.5 }}
+                className={styles.subtitle}
+            >
+                Découvrez moi et mes créations dans mon monde digital.
+            </motion.p>
         </div>
     );
 }
