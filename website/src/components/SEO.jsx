a80f4b114eabafb569467d4a00631ddaa64ddd7f
Fenerz07 <hugom2005@free.fr>
2025-02-28 16:24:10 +0100

    🐛| #12 | SEO component: update favicon path and streamline return statement for improved readability

--- website/src/components/SEO.jsx
+++ website/src/components/SEO.jsx
@@ -1,18 +1,18 @@
 import Head from "next/head";
 
 const SEO = ({ title, description }) => {
-  return (
+return (
     <Head>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-      <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
-      <meta name="theme-color" content="#1A1A1A" media="(prefers-color-scheme: dark)" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/images/favicon.ico" />
+        <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
+        <meta name="theme-color" content="#1A1A1A" media="(prefers-color-scheme: dark)" />
     </Head>
-  );
+);
 };
 
 export default SEO;
